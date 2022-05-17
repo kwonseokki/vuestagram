6f3bb40065c8e1d9d3fb095302f18c3e1277098b
seokki kwon <kwons4163@naver.com>
2022-05-18 01:36:35 +0900

    axios 더보기 버튼만들기

---
+++ src/components/ContainerPost.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <DoPost v-for="(x,y) in postData" :key="y" :postData="postData[y]"></DoPost>
+</div>
+
+</template>
+
+<script>
+import DoPost from './DoPost.vue'
+export default {
+    name : "ContainerPost",
+    data (){
+        return{ 
+           
+        }
+    },
+    components : {
+        DoPost,
+    },
+    props : {
+      postData:Object
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/DoPost.vue
@@ -0,0 +1,74 @@
+<template>
+
+  <div class="post">
+    <div class="post-header">
+      <div class="profile" :style="{background:`url(${postData.userImage})`}">
+      </div>
+      <span class="profile-name">{{postData.name}}</span>
+    </div>
+    <div class="post-body" :style="{background : `url(${postData.postImage})`}">
+     
+    </div>
+    <div class="post-content">
+      <p>{{postData.likes}}</p>
+      <p><strong>{{postData.name}}</strong>{{postData.content}}</p>
+      <p class="date">May 15</p>
+    </div>
+</div> 
+</template>
+
+<script>
+
+export default {
+    name : "DoPost",
+    data() {
+
+    },
+    props : {
+       postData:Object
+    }
+}
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-size:cover;
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+  overflow:hidden;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://placeimg.com/640/480/animals");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+  overflow:hidden;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+} 
+</style>
