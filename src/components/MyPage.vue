497b08063d007c949248fa98e9432d669b5f89a0
seokki kwon <kwons4163@naver.com>
2022-06-06 01:07:54 +0900

    컴포지션 api2

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -5,28 +5,40 @@
   <div class="post-header" v-for=" (x,y) in follower" :key="y">
     <div class="profile" :style="{background:`url(${follower[y].image})`}"></div>
     <span class="profile-name">{{follower[y].name}}</span>
+    
   </div>
  
 </div>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref,  toRefs,} from 'vue';
 import axios from 'axios';
+import { useStore } from 'vuex';
 export default {
     name : "MyPage",
-    setup() {
+    setup(props) {
         let follower = ref([]);
+       let {one} =  toRefs(props);
 
+      let result = computed(()=>{return follower.value.length;});
+      console.log(result.value);
+      console.log(one.value);
+
+      let store = useStore();
+      console.log(store.state.name);
+      
         onMounted(()=> {
              axios.get('/follower.json')
         .then((res)=>{ follower.value = res.data});
         })
-
           return { follower };
     },
     data() {
         return {}
+    },
+    props : {
+      one : Number
     }
 }
 </script>
